bd9fd3776735efce067c7af36b4a588eaf7de120
Fausto Licardié <flicardie@gmail.com>
2018-07-25 15:18:37 -0600

    Archivos iniciales para reconocimiento facial

---
+++ ReconocimientoFacial/FrmPrincipal.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ReconocimientoFacial
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageBoxCamaraNormal = new ReconocimientoFacial.ImageBox();
+            this.imageBoxCamaraConDeteccion = new ReconocimientoFacial.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxCamaraNormal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxCamaraConDeteccion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(23, 27);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(102, 23);
+            this.captureButton.TabIndex = 1;
+            this.captureButton.Text = "Start Capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.CapturarVideoClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cámara Normal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(618, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cámara con detección";
+            // 
+            // imageBoxCamaraNormal
+            // 
+            this.imageBoxCamaraNormal.FunctionalMode = ((ReconocimientoFacial.ImageBox.FunctionalModeOption)((ReconocimientoFacial.ImageBox.FunctionalModeOption.RightClickMenu | ReconocimientoFacial.ImageBox.FunctionalModeOption.PanAndZoom)));
+            this.imageBoxCamaraNormal.Location = new System.Drawing.Point(23, 117);
+            this.imageBoxCamaraNormal.Name = "imageBoxCamaraNormal";
+            this.imageBoxCamaraNormal.Size = new System.Drawing.Size(529, 395);
+            this.imageBoxCamaraNormal.TabIndex = 3;
+            this.imageBoxCamaraNormal.TabStop = false;
+            // 
+            // imageBoxCamaraConDeteccion
+            // 
+            this.imageBoxCamaraConDeteccion.FunctionalMode = ((ReconocimientoFacial.ImageBox.FunctionalModeOption)((ReconocimientoFacial.ImageBox.FunctionalModeOption.RightClickMenu | ReconocimientoFacial.ImageBox.FunctionalModeOption.PanAndZoom)));
+            this.imageBoxCamaraConDeteccion.Location = new System.Drawing.Point(621, 117);
+            this.imageBoxCamaraConDeteccion.Name = "imageBoxCamaraConDeteccion";
+            this.imageBoxCamaraConDeteccion.Size = new System.Drawing.Size(529, 395);
+            this.imageBoxCamaraConDeteccion.TabIndex = 2;
+            this.imageBoxCamaraConDeteccion.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBoxCamaraNormal);
+            this.Controls.Add(this.imageBoxCamaraConDeteccion);
+            this.Controls.Add(this.captureButton);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxCamaraNormal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxCamaraConDeteccion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button captureButton;
+        private ImageBox imageBoxCamaraConDeteccion;
+        private ImageBox imageBoxCamaraNormal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ReconocimientoFacial/FrmPrincipal.cs
@@ -0,0 +1,163 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace ReconocimientoFacial
+{
+    public partial class FrmPrincipal : Form
+    {
+
+        private VideoCapture _capture = null;
+        private bool _captureInProgress;
+        private Mat _frame;
+        private Mat _grayFrame;
+        private Mat _smallGrayFrame;
+        private Mat _smoothedGrayFrame;
+        private Mat _cannyFrame;
+
+        CascadeClassifier _cascadeClassifier1 = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
+        CascadeClassifier _cascadeClassifier2 = new CascadeClassifier(Application.StartupPath + "/haarcascade_eye.xml");
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new VideoCapture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            _frame = new Mat();
+            _grayFrame = new Mat();
+            _smallGrayFrame = new Mat();
+            _smoothedGrayFrame = new Mat();
+            _cannyFrame = new Mat();
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+
+                CvInvoke.CvtColor(_frame, _grayFrame, ColorConversion.Bgr2Gray);
+
+                //CvInvoke.PyrDown(_grayFrame, _smallGrayFrame);
+
+                //CvInvoke.PyrUp(_smallGrayFrame, _smoothedGrayFrame);
+
+                //CvInvoke.Canny(_smoothedGrayFrame, _cannyFrame, 100, 60);
+
+
+                //captureImageBox.Image = _frame;
+                imageBoxCamaraNormal.Image = _frame;
+                //pictureBoxCamaraNormal.Image = _frame.Bitmap;
+                //imageBoxCamaraConDeteccion.Image = _grayFrame;
+                //smoothedGrayscaleImageBox.Image = _smoothedGrayFrame;
+                //cannyImageBox.Image = _cannyFrame;
+
+                DetectarOjosYCara(_grayFrame);
+            }
+        }
+
+        private void CapturarVideoClick(object sender, EventArgs e)
+        {
+
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    captureButton.Text = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    captureButton.Text = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+
+        }
+
+        private void ReleaseData()
+        {
+            if (_capture != null)
+                _capture.Dispose();
+        }
+
+        private void FlipHorizontalButtonClick(object sender, EventArgs e)
+        {
+            if (_capture != null) _capture.FlipHorizontal = !_capture.FlipHorizontal;
+        }
+
+        private void FlipVerticalButtonClick(object sender, EventArgs e)
+        {
+            if (_capture != null) _capture.FlipVertical = !_capture.FlipVertical;
+        }
+
+        private void DetectarOjosYCara(Mat image)
+        {
+
+            //convert Mat image to Emgu.CV RGB Image
+            using (var imageFrame = image.ToImage<Bgr, byte>())
+            {
+                if (imageFrame != null)
+                {
+                    //convert Emgu.CV RGB Image to Emgu.CV GRAY Image 
+                    var grayframe = imageFrame.Convert<Gray, byte>();
+
+                    List<Rectangle> faces = new List<Rectangle>();
+                    List<Rectangle> eyes = new List<Rectangle>();
+                    //the actual face detection happens here
+                    long detectionTime;
+                    DetectorDeCaras.Detect(
+                      imageFrame, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
+                      faces, eyes,
+                      out detectionTime);
+
+                    //LBPHFaceRecognizer faceRecognizer = new LBPHFaceRecognizer();
+
+
+
+
+                    //the detected face(s) are highlighted here using a box that is drawn around it/them
+                    foreach (var face in faces)
+                    {
+                        imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3);
+                    }
+
+                    //the detected eye(s) are highlighted here using a box that is drawn around it/them
+                    foreach (var eye in eyes)
+                    {
+                        imageFrame.Draw(eye, new Bgr(Color.Red), 1);
+                    }
+                }
+
+
+
+                //display dected face and eyes Image on ImageBox
+                imageBoxCamaraConDeteccion.Image = imageFrame; 
+                //pictureBoxCamaraNormal.Image = imageFrame.Bitmap;
+            }
+        }
+
+    }
+}
